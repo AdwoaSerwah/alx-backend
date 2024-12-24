2555e168050efec6078151f70f3c1c3418402cdd
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-12-24 15:04:49 +0000

    Add task 3 again

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """
-This script sets up a basic Flask app with Babel for localization and selects the locale from the request.
+This script sets up a basic Flask app with Babel for localization and
+selects the locale from the request.
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 # Set up the Flask app
 app = Flask(__name__)
+
 
 # Config class to set up the app configuration
 class Config:
@@ -18,11 +20,13 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 # Apply the config to the Flask app
 app.config.from_object(Config)
 
 # Instantiate the Babel object
 babel = Babel(app)
+
 
 @app.route('/', strict_slashes=False)
 def index():
@@ -31,13 +35,16 @@ def index():
     """
     return render_template('3-index.html')
 
+
 @babel.localeselector
 def get_locale():
     """
     Determine the best match with the client's preferred language
     based on the request's Accept-Language header.
     """
-    return request.args.get('locale', request.accept_languages.best_match(app.config['LANGUAGES']))
+    return request.args.get(
+        'locale', request.accept_languages.best_match(app.config['LANGUAGES']))
+
 
 if __name__ == '__main__':
     # Run the app
