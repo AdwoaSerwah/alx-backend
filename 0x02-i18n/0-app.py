528fafa3f6f42a48151f4cdcb5bb30b327557a27
AdwoaSK <106457557+AdwoaSK@users.noreply.github.com>
2024-12-24 13:24:39 +0000

    Add task 0 again

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python3
-"""
-This script sets up a basic Flask app with a single route.
-"""
+"""Basic Flask app module"""
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """
-    Render the index page with 'Welcome to ALX' title and
-    'Hello world' header.
-    """
+@app.route('/')
+def get_index() -> str:
+    """index page"""
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    # Make the app accessible on all network interfaces, and
-    # allow access with or without a trailing slash
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
