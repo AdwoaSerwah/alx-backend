0a6270256e832afe6f08e24399938b78f87d5ef5
AdwoaSK <106457557+AdwoaSK@users.noreply.github.com>
2024-12-24 13:28:55 +0000

    Add task 0 again 1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-"""Basic Flask app module"""
+"""
+This script sets up a basic Flask app with a single route.
+"""
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def get_index() -> str:
-    """index page"""
+def index():
+    """
+    Render the index page with 'Welcome to ALX' title and
+    'Hello world' header.
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
